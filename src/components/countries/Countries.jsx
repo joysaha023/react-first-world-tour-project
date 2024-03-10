d50efd3e0f234c8b9e37884b69987b750ed11e90
Joy <github.joysaha@gmail.com>
2024-03-10 21:03:12 +0600

    functionality added and fetch data

---
+++ src/components/countries/Countries.jsx
@@ -0,0 +1,14 @@
+import { useState } from "react";
+
+
+const Countries = () => {
+    const [countries, setCountries] = useState([]);
+
+    return (
+        <div>
+            <h3>Countries</h3>
+        </div>
+    );
+};
+
+export default Countries;
